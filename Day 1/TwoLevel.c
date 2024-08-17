8799f004e7c54d122ee772de6ac36157a71f0937
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-08-17 17:39:49 +0530

    Update TwoLevel.c

--- Day 1/TwoLevel.c
+++ Day 1/TwoLevel.c
@@ -62,7 +62,7 @@ void display () {
 	}
 }
 
-void delete () {
+void deleteFile () {
     char filename[30];
     
 	if (head == NULL) {
@@ -93,6 +93,37 @@ void delete () {
 	} // else	
 }
 
+void deleteDir () {
+    char dirname[30];
+    
+    if (head == NULL) {
+        printf ("no elements\n");
+    } else {
+        ptr = head;
+        printf ("Enter the name of the directory: ");
+        scanf (" %s", dirname);
+        
+         while ((ptr != NULL) && (strcmp(dirname,ptr->dname) != 0)) {
+        	prev = ptr;
+            ptr = ptr->next;
+        }
+        
+        if (strcmp(dirname, ptr->dname) == 0) {
+        	if (ptr == head) {
+        		head = ptr->next;
+        		free(ptr);
+        	} else if (ptr == tail) {
+        		prev = tail;
+        		prev->next = NULL;
+        		free(ptr);
+        	} else {
+        		prev->next = ptr->next;
+        		free(ptr);
+        	}
+        }
+    }
+}
+
 void search () {
 	char filename[20];
 	printf ("Enter the file to search: ");
@@ -117,7 +148,7 @@ int main () {
 	int choice = -1;
 	char file[20], directory[20];
 	
-	printf("MENU:\n1. Create\t2. Delete\n3. Search\t4. Display\n5. Exit\n");
+	printf("MENU:\n1. Create\t\t\t2. Delete File\n3. Delete Directory\t4. Search\n5. Display\t\t\t6. Exit\n");
 	
 	while (1) {
 		printf ("\nEnter choice: ");
@@ -127,18 +158,22 @@ int main () {
 			case 1: insert();
 					break;
 					
-			case 2: delete();
+			case 2: deleteFile();
 					printf ("File has been deleted successfully...\n");
 					break;
-					
-			case 3: search(); 
+			
+			case 3: deleteDir();
+			        printf ("Directory has been deleted successfully...\n");
 					break;
 					
-			case 4: display();
+			case 4: search(); 
+					break;
+					
+			case 5: display();
 					printf ("Files have been displayed successfully...\n");
 					break;
 			
-			case 5: printf ("Terminated...\n");
+			case 6: printf ("Terminated...\n");
 					return 0;
 					
 			default: printf ("Wrong choice...\n");
