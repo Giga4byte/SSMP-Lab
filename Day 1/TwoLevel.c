e2d81326fadb005c0c63b92f843a9f11c64b3296
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-09-02 22:05:18 +0530

    Update TwoLevel.c

--- Day 1/TwoLevel.c
+++ Day 1/TwoLevel.c
@@ -92,34 +92,43 @@ void deleteFile () {
 	} // else	
 }
 
-void deleteDir () {
+void deleteDir() {
     char dirname[30];
+    int found = 0;
     
     if (head == NULL) {
-        printf ("no elements\n");
-    } else {
-        ptr = head;
-        printf ("Enter the name of the directory: ");
-        scanf (" %s", dirname);
-        
-         while ((ptr != NULL) && (strcmp(dirname,ptr->dname) != 0)) {
-        	prev = ptr;
+        printf("no elements\n");
+        return;
+    }
+
+    printf("Enter the name of the directory: ");
+    scanf(" %s", dirname);
+
+    ptr = head; prev = NULL;
+
+    while (ptr != NULL) {
+        if (strcmp(ptr->dname, dirname) == 0) {
+            found = 1;
+            printf("Deleting file %s...\n", ptr->fname);
+
+            if (ptr == head) {
+                head = ptr->next;
+                free(ptr);
+                ptr = head;
+            } else {
+                prev->next = ptr->next;
+                free(ptr);
+                ptr = prev->next;
+            }
+        } else {
+            prev = ptr;
             ptr = ptr->next;
         }
-        
-        if (strcmp(dirname, ptr->dname) == 0) {
-        	if (ptr == head) {
-        		head = ptr->next;
-        		free(ptr);
-        	} else if (ptr == tail) {
-        		prev = tail;
-        		prev->next = NULL;
-        		free(ptr);
-        	} else {
-        		prev->next = ptr->next;
-        		free(ptr);
-        	}
-        }
+    }
+    if (!found) {
+        printf("Directory %s not found or no files in the directory.\n", dirname);
+    } else {
+        printf("Directory %s and all its files have been deleted...\n", dirname);
     }
 }
 
@@ -140,14 +149,12 @@ void search () {
     }
 }
 
-
-
 int main () {
 	head = NULL; tail = NULL;
 	int choice = -1;
 	char file[20], directory[20];
 	
-	printf("MENU:\n1. Create\t\t\t2. Delete File\n3. Delete Directory\t4. Search\n5. Display\t\t\t6. Exit\n");
+	printf("MENU:\n1. Create\t\t\t2. Delete File\n3. Delete Direc\t\t4. Search\n5. Display\t\t\t6. Exit\n");
 	
 	while (1) {
 		printf ("\nEnter choice: ");
@@ -162,7 +169,7 @@ int main () {
 					break;
 			
 			case 3: deleteDir();
-			        printf ("Directory has been deleted successfully...\n");
+			        //printf ("Directory has been deleted successfully...\n");
 					break;
 					
 			case 4: search(); 
