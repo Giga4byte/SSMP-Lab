e3ee41ec3de0b1d60d7c586544056b129d172d2d
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-09-23 19:46:37 +0530

    Update Addition.asm

--- Day 4/Addition.asm
+++ Day 4/Addition.asm
@@ -1,1 +1,29 @@
 .model small
+.data
+a dw 3027h
+b dw 40FAh
+.code
+mov ax, @data
+mov ds, ax
+mov si, offset a
+mov bx, [si]
+mov di, offset b
+mov ax, [di]
+add bx
+mov cx, 0000h
+mov bx, 000Ah
+L: mov dx, 0000h
+div bx
+push dx
+inc cx
+cmp ax, 0000h
+jnz L
+D: pop dx
+add dl, 30h
+mov ah, 02h
+int 21h
+dec cl
+jnz D
+mov ah, 4ch
+int 21h
+end
