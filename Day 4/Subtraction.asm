9f5ef4c3f70d65c3bd41888db826c122ab4a4037
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-09-26 19:38:55 +0530

    Update Subtraction.asm

--- Day 4/Subtraction.asm
+++ Day 4/Subtraction.asm
@@ -1,30 +1,32 @@
 .model small
 .stack 100h
 .data
-a dw FF30h
-b dw 0030h
 .code
-mov ax, @data
-mov ds, ax
-mov si, offset b
-mov bx, [si]
-mov si, offset a
-mov ax, [si]
-sub bx
-mov cx, 0000h
-mov bx, 000Ah
-L: mov dx, 0000h
-div bx
-push dx
-inc cx
-cmp ax, 0000h
-jnz L
-D: pop dx
-add dl, 30h
-mov ah, 02h
-int 21h
-dec cl
-jnz D
-mov ah, 4ch
-int 21h
+  mov ax, @data
+  mov ds, ax
+  mov bx, 3333h
+  mov ax, 5555h
+  sub ax, bx
+  mov cx, 0000h
+  mov bx, 16
+convert:
+    xor dx, dx
+    div bx
+    push dx
+    inc cx
+    cmp ax, 0
+    jnz convert
+printloop:
+    pop dx
+    add dx, '0'
+    cmp dx, '9'
+    jbe output
+    add dx, 7
+output:
+    mov ah, 02h
+    int 21h
+    dec cx
+    jnz printloop
+  mov ah, 4ch
+  int 21h
 end
