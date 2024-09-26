9203af4fcb3046ec079700b45606049d29ad8ec9
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-09-26 19:37:59 +0530

    Update Addition.asm

--- Day 4/Addition.asm
+++ Day 4/Addition.asm
@@ -1,30 +1,36 @@
 .model small
 .stack 100h
 .data
-a dw 3027h
-b dw 40FAh
+  a dw 1234h
+  b dw 5678h
 .code
-mov ax, @data
-mov ds, ax
-mov si, offset a
-mov bx, [si]
-mov di, offset b
-mov ax, [di]
-add bx
-mov cx, 0000h
-mov bx, 000Ah
-L: mov dx, 0000h
-div bx
-push dx
-inc cx
-cmp ax, 0000h
-jnz L
-D: pop dx
-add dl, 30h
-mov ah, 02h
-int 21h
-dec cl
-jnz D
-mov ah, 4ch
-int 21h
+  mov ax, @data
+  mov ds, ax
+  mov si, offset a
+  mov bx, [si]
+  mov di, offset b
+  mov ax, [di]
+  add ax, bx
+  mov cx, 0000h
+  mov bx, 16
+convert:
+    xor dx, dx
+    div bx
+    push dx
+    inc cx
+    cmp ax, 0
+    jnz convert
+printloop:
+    pop dx
+    add dx, '0'
+    cmp dx, '9'
+    jbe output
+    add dx, 7
+output:
+    mov ah, 02h
+    int 21h
+    dec cx
+    jnz printloop
+  mov ah, 4ch
+  int 21h
 end
