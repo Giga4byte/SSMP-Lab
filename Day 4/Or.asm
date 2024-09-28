fca81d1a3c177d3c777844bc97afa4d5a4470bc3
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-09-28 18:35:45 +0530

    Create Or.asm

---
+++ Day 4/Or.asm
@@ -0,0 +1,39 @@
+.model small
+.stack 100h
+.data
+  a dw 0110h
+  b dw 1001h
+.code
+  mov ax, @data
+  mov ds, ax
+  mov si, offset a
+  mov bx, [si]
+  mov di, offset b
+  mov ax, [di]
+  or ax, bx
+  mov cx, 0
+  mov bx, 10
+  
+  test ax, ax 
+  jnz convert
+  mov dx, 0  
+  add dx, 30h   
+  mov ah, 02h
+  int 21h            
+  jmp endProg
+convert:
+    xor dx, dx
+    div bx
+    push dx
+    inc cx
+    cmp ax, 0
+    jnz convert
+printloop:
+    pop dx
+    add dx, 30h
+    mov ah, 02h
+    int 21h
+endProg: 
+    mov ah, 4ch
+    int 21h
+end
