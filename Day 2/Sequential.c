6be0173cd57f86669d1989d9e125b797d1470a77
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-10-01 21:34:31 +0530

    Update Sequential.c

--- Day 2/Sequential.c
+++ Day 2/Sequential.c
@@ -5,42 +5,57 @@
 struct node {
     char name[30];
     int blocks;
-    int size;
+    int allocatedBlocks[20];
     struct node *next;
 };
 struct node *head, *tail, *ptr, *p;
 
-void allocateFile (int totalBlocks) {
+void allocateFile(int totalBlocks, int status[]) {
     p = (struct node*)malloc(sizeof(struct node));
-    printf ("Enter the name of the file: ");
-    scanf (" %s", p->name);
-    printf ("Enter the no of blocks for it: ");
-    scanf ("%d", &p->blocks);
-    
-    int available = totalBlocks;
-    ptr = head;
-    while (ptr != NULL) {
-        available -= ptr->blocks;
-        ptr = ptr->next;
+    printf("Enter the name of the file: ");
+    scanf(" %s", p->name);
+    printf("Enter the number of blocks for it: ");
+    scanf("%d", &p->blocks);
+
+    int freeBlocks = 0;
+    for (int i = 0; i < totalBlocks; i++) {
+        if (status[i] == 0) {
+            freeBlocks++;
+        }
     }
-    
-    if (p->blocks > available) {
+
+    if (p->blocks > freeBlocks) {
         printf("\tFile %s cannot be allocated...\n", p->name);
         free(p);
         return;
     }
-    
+
+    int allocated = 0, index = 0;
+    for (int i = 0; i < totalBlocks && allocated < p->blocks; i++) {
+        if (status[i] == 0) {
+            if (index != p->blocks - allocated) {
+                p->allocatedBlocks[allocated] = i;
+                status[i] = 1;
+                allocated++;
+            }
+        }
+    }
+
     if (head == NULL) {
-        p->size = p->blocks;  
+        p->next = NULL;
         head = tail = p;
     } else {
-        p->size = tail->size + p->blocks;
+        p->next = NULL;
         tail->next = p;
         tail = p;
     }
-    p->next = NULL;
 
-    printf("\tFile %s has been allocated.\n\tRemaining blocks = %d\n", p->name, available - p->blocks);
+    printf("\tFile %s has been allocated.\n", p->name);
+    printf("\tBlocks allocated: ");
+    for (int i = 0; i < allocated; i++) {
+        printf("%d ", p->allocatedBlocks[i]);
+    }
+    printf("\n");
 }
 
 int main () {
@@ -50,12 +65,17 @@ int main () {
     printf("Enter the total number of blocks: ");
     scanf("%d", &totalBlocks);
     
+    int status[totalBlocks];
+    for (int i = 0; i < totalBlocks; i++) {
+        status[i] = 0;
+    }
+    
     while (1) {
         printf("Do you want to enter a file: ");
         scanf(" %s", choice);
         
         if (strcmp(choice, "yes") == 0) {
-            allocateFile(totalBlocks);
+            allocateFile(totalBlocks, status);
         } else if (strcmp(choice, "no") == 0) {
             printf("\tTerminating...\n");
             return 0;
