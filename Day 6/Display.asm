0d22cb6ef61ca5ae5910007d2e19a16b25b678a0
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2024-10-22 23:00:52 +0530

    Update Display.asm

--- Day 6/Display.asm
+++ Day 6/Display.asm
@@ -10,3 +10,27 @@
     mov ah, 09h
     int 21h
 end
+
+;alternate code
+; giri6.asm
+
+.model small
+.stack 100h
+.data
+    msg db 'MASM Display$'
+.code
+    mov ax, @data 
+    mov ds, ax
+    lea si, msg  
+access:
+    mov dl, [si] 
+    cmp dl, '$'
+    jz exiting    
+    mov ah, 02h
+    int 21h     
+    inc si   
+    jmp access           
+exiting:
+    mov ah, 4Ch 
+    int 21h 
+end
